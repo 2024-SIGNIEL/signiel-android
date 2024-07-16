b587609413a62d3874ac71c3cc048166688506c3
Tmdhoon2 <hks026naver@gmail.com>
2024-07-16 16:32:25 +0900

    feat :: SignielTextField

--- core/designsystem/src/main/java/com/seunghoon/designsystem/ui/theme/Colors.kt
+++ core/designsystem/src/main/java/com/seunghoon/designsystem/ui/theme/Colors.kt
@@ -7,4 +7,7 @@ object Colors {
     val White = Color(0xFFFFFFFF)
     val Black = Color(0xFF000000)
     val Transparent = Color(0x00000000)
+    val Gray = Color(0xFF9DA3AF)
+    val Main = Color(0xFF3F69FF)
+    val Error = Color(0xFFD10000)
 }
--- core/designsystem/src/main/java/com/seunghoon/designsystem/ui/theme/Typography.kt
+++ core/designsystem/src/main/java/com/seunghoon/designsystem/ui/theme/Typography.kt
@@ -1,7 +1,11 @@
 package com.seunghoon.designsystem.ui.theme
 
 import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 // 디자인에 맞는 폰트 추가해서 사용
 private val FontFamily = FontFamily(listOf())
@@ -19,4 +23,10 @@ object Typography {
         lineHeight = 28.sp,
         platformStyle = platFormTextStyle,
     )*/
+    val Medium = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp,
+        //platformStyle = platFormTextStyle,
+    )
 }
