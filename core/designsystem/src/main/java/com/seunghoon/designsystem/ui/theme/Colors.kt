3e44b2b5e8dc0ef943c6609d0b15b44d52001d29
Tmdhoon2 <hks026naver@gmail.com>
2024-07-16 17:26:13 +0900

    feat :: SignInScreen

--- core/designsystem/src/main/java/com/seunghoon/designsystem/ui/theme/Colors.kt
+++ core/designsystem/src/main/java/com/seunghoon/designsystem/ui/theme/Colors.kt
@@ -8,6 +8,8 @@ object Colors {
     val Black = Color(0xFF000000)
     val Transparent = Color(0x00000000)
     val Gray = Color(0xFF9DA3AF)
+    val Gray2 = Color(0xFFE8E8E8)
     val Main = Color(0xFF3F69FF)
+    val Main_Pressed = Color(0xFF1145FF)
     val Error = Color(0xFFD10000)
 }
