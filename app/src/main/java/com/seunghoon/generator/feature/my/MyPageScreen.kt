e317d28ebcdf3b728e11a8bd4a163b189df9f3d8
uiel <parkuiery@gmail.com>
2024-07-17 22:55:58 +0900

    feat :: mypage

--- app/src/main/java/com/seunghoon/generator/feature/my/MyPageScreen.kt
+++ app/src/main/java/com/seunghoon/generator/feature/my/MyPageScreen.kt
@@ -1,18 +1,85 @@
 package com.seunghoon.generator.feature.my
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.seunghoon.core.designsystem.R
 
 @Composable
 fun MyPageScreen(
     modifier: Modifier = Modifier,
+    navController: NavController,
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
     ) {
+        Text(
+            modifier = Modifier
+                .padding(
+                    vertical = 20.dp,
+                    horizontal = 24.dp,
+                ),
+            text = "마이페이지",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    vertical = 20.dp,
+                    horizontal = 24.dp,
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(90.dp)
+                    .clip(CircleShape),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.width(30.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "홍길동",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                    )
+                    Text(
+                        text = "님, 안녕하세요",
+                        fontSize = 16.sp,
+                    )
+                }
+                Text(
+                    text = "abddsas@gmail, wlqdprkryhtlvek",
+                    fontSize = 12.sp,
+                )
+            }
+        }
 
     }
 }
